1e78d720b2d54abaa495ce2bdb540e8f25586812
Noah Tilton <noah@tilton.co>
2015-06-18 11:25:03 -0500

    blah

--- htpasswd.rb
+++ htpasswd.rb
@@ -1,3 +1,22 @@
 #!/usr/bin/env ruby
-#
-puts ARGV[0].crypt("salt")
+require 'webrick'
+ 
+if ARGV.size < 3
+    puts "usage: #{$0} password-file user password"
+      exit 1
+end
+ 
+# create htpassword instance
+passwordfile = ARGV.shift
+passwd = WEBrick::HTTPAuth::Htpasswd.new(passwordfile)
+ 
+# register password
+user = ARGV.shift
+password = ARGV.shift
+passwd.set_passwd(nil, user, password)
+passwd.flush # save
+puts IO.read(passwordfile)
+ 
+# get crypted password
+enc_pass = passwd.get_passwd(nil, user, false)
+puts enc_pass
